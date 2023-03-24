1e5ca415f54ce1865e09a64933fe2bba6efe0889
antonrejoe <antonrejoe@gmail.com>
2023-03-24 18:15:37 +0530

    successfully I know how to create a document

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -1,72 +1,65 @@
-import React from 'react'
-import { Link, useNavigate } from 'react-router-dom'
-import {account}  from "../appwrite/appwriteConfig.ts";
-import { useState , useEffect } from 'react';
-import Signup from './Signup';
+import React from "react";
+import { Link, useNavigate } from "react-router-dom";
+import { account } from "../appwrite/appwriteConfig.ts";
+import { useState, useEffect } from "react";
+import Signup from "./Signup";
 
-import "../assets/css/Profile/index.css"
-import TodoForm from './TodoForm';
+import "../assets/css/Profile/index.css";
+import TodoForm from "./TodoForm";
 
 const Profile = () => {
-
   const navigate = useNavigate();
 
   const [userDetails, setuserDetails] = useState();
   console.log(userDetails);
 
-  
   useEffect(() => {
     const getData = account.get();
 
     getData.then(
-      function(response) {
-        setuserDetails(response)
-
+      function (response) {
+        setuserDetails(response);
       },
       function (error) {
         console.log(error);
       }
-    )
-  
-    
-  }, [])
+    );
+  }, []);
 
- const handleLogout = async () => { 
-
-  try {
-    await account.deleteSession("current");
-    navigate("/login");
-  } catch (error) {
-    console.log(error);
-  }
- }
+  const handleLogout = async () => {
+    try {
+      await account.deleteSession("current");
+      navigate("/login");
+    } catch (error) {
+      console.log(error);
+    }
+  };
 
   return (
     <>
-    {userDetails ? (
-      <>
-      <div class="navbar">
-        <h3>{userDetails.name}</h3>
-        <button class='logout_btn' onClick={handleLogout}>Logout</button>
-      </div>
-      <TodoForm/>
-
-      </>
-
-    ):(
-      <>
-      <div class='signUp_direct'>
-        <img src="/public/Ancient-Aliens.jpg" alt="Ancient aliens were real" />
-          <Link to="/signup">consider Signing in or else aliens will take you away</Link>
-          <p> </p>
-      </div>
-        
-      </>
-
-    )}
-    
+      {userDetails ? (
+        <>
+          <div class="navbar">
+            <h3>{userDetails.name}</h3>
+            <button class="logout_btn" onClick={handleLogout}>
+              Logout
+            </button>
+          </div>
+          <TodoForm />
+        </>
+      ) : (
+        <>
+          <div class="signUp_direct">
+            <img src="/Ancient-Aliens.jpg" alt="Ancient aliens were real" />
+            <Link to="/signup">
+              consider Signing in or else aliens will take you away
+            </Link>
+            <p> </p>
+          </div>
+        </>
+      )}
     </>
-  )
-}
+  );
+};
 
-export default Profile
+export default Profile;
--- src/components/TodoForm.jsx
+++ src/components/TodoForm.jsx
@@ -1,55 +1,70 @@
-import React from 'react'
-import { useState } from 'react'
-import {account} from "../appwrite/appwriteConfig.ts"
-import {databases} from "../appwrite/appwriteConfig"
-import  "../assets/css/TodoForm/todoForm.css";
-import {v4 as uuvidV4} from 'uuid';
-import { Permission ,Role } from 'appwrite';
+import React from "react";
+import { useState, useEffect } from "react";
+import { account } from "../appwrite/appwriteConfig.ts";
+import { databases } from "../appwrite/appwriteConfig";
+import "../assets/css/TodoForm/todoForm.css";
+
 const TodoForm = () => {
+  const [todo, setTodo] = useState({});
+  useEffect(() => {
+    const promise = account.get();
 
-  const [todo, setTodo] = useState({
-    "author": account.name,
-    "todo":''
-  })
-  const handleSubmit = (e) =>{
-
-    e.preventDefault();  
-    const promise  = databases.createDocument("64187e3b0c4e988035e2", "64187e5258a86452f64d" , uuvidV4() , todo.todo , [
-      Permission.read(Role.any()),
-      Permission.update(Role.any()),
-    ] )
     promise.then(
       function (response) {
-        console.log(response);  
-      },function (error) {
+        setTodo(response);
+      },
+      function (error) {
         console.log(error);
       }
-    )
-  }
+    );
+  }, []);
 
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const promise = databases.createDocument(
+      "64187e3b0c4e988035e2",
+      "64187e5258a86452f64d",
+      crypto.randomUUID(),
+      {
+        content: todo.content,
+      }
+    );
+
+    promise.then(
+      function (response) {
+        console.log(response);
+      },
+      function (error) {
+        console.log(error);
+      }
+    );
+  };
 
   return (
-    <>  
-      <div>TodoForm</div>
-
-      <form>
-        <input type="text" id="add_todo" placeholder='moshi moshi ' name='add_todo'
-          onChange={(e) =>{
-
-            setTodo({...todo ,[e.target.todo]: e.target.value})
-            console.log(todo);
-          }}
-        />
-        <button for="add_todo" class="add_todo_btn" onClick={handleSubmit}>add</button>  
-
-
-      </form>
-      <div>
+    <>
+      <div class="todo_form_container">
+        <form class={"todo_form"}>
+          <input
+            class="todo_add_input"
+            type="text"
+            id="add_todo"
+            placeholder="moshi moshi "
+            name="add_todo"
+            onChange={(e) => {
+              setTodo(() => ({
+                ...todo,
+                ...(todo.content = e.target.value),
+              }));
+            }}
+          />
+          <button for="add_todo" class="add_todo_btn" onClick={handleSubmit}>
+            add Todo
+          </button>
+        </form>
+        <div></div>
       </div>
-      
     </>
-   
-  )
-}
+  );
+};
 
-export default TodoForm
+export default TodoForm;
